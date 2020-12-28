cbc86fe1d5cb1dfb9cbf425c16b3180ed1fac34f
Frank-Top <3175973454@qq.com>
2020-12-28 17:26:52 +0800

    测试

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,8 @@
 public class test1 {
     public static void main(String[] args) {
+        String name = "Frank";
+        System.out.println(name);
+        System.out.println("- - - - - -");
         System.out.println("hahaha");
     }
 }
